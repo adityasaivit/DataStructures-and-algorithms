bd13b646b69c5852d984b84f54ba5f6bc5484403
adityasaivit <113006142+adityasaivit@users.noreply.github.com>
2024-12-01 13:45:16 +0530

    Create linkedlist_get_set.js

---
+++ Linkedlist/linkedlist_get_set.js
@@ -0,0 +1,162 @@
+class node{
+    constructor(val)
+    {
+        this.val=val
+        this.next=null
+    }
+}
+
+class linkedlist{
+    constructor()
+    {
+        this.head=null
+        this.tail=null
+        this.length=0;
+    }
+
+    push(v)
+    {
+        if(!this.head)
+        {
+            this.head=new node(v)
+            this.tail=this.head
+        }
+        else{
+            this.tail.next=new node(v)
+            this.tail=this.tail.next
+            
+        }
+        this.length=this.length+1
+
+
+    }
+
+    pop()
+    {
+        if(!this.head)
+        {
+            return undefined
+        }
+        else{
+            let temp=this.head
+            while(temp.next.next!=null)
+            {
+                temp=temp.next
+            }
+            temp.next=null
+            this.tail=temp
+            this.length=this.length-1
+            if(this.length==0)
+                {
+                    this.head=null
+                    this.tail=null
+                }
+
+        }
+
+
+    }
+
+    shift()
+    {
+        if(!this.head)
+        {
+            return undefined
+
+        }
+        else{
+            this.head=this.head.next;
+        }
+
+        this.length=this.length-1
+        if(this.length==0)
+        {
+            this.head=null
+            this.tail=null
+        }
+    }
+
+    unshift(n)
+    {
+        let newnode=new node(n)
+        if(!this.head)
+        {
+            this.head=new node(n)
+            this.tail=this.head
+        }
+        else{
+            newnode.next=this.head;
+            this.head=newnode
+        }
+
+        this.length=this.length+1
+
+    }
+
+    traverse()
+    {
+       let temp=this.head
+
+       while(temp)
+       {
+            console.log(temp.val)
+            temp=temp.next
+       }
+        
+    }
+
+    get(i)
+    {
+        if(!this.head || i<0 || i>=this.length)
+        {
+            return undefined
+        }
+        
+        let first=0
+        let temp=this.head
+        while(first!=i)
+        {
+            temp=temp.next
+            first=first+1
+        }
+        console.log("get value is ")
+        console.log(temp.val)
+        return temp
+    }
+
+    set(i,val)
+    {
+        let temp=this.get(i)
+        if(temp==undefined)
+        {
+            return undefined
+        }
+        else{
+            temp.val=val;
+        }
+    }
+
+
+}
+
+
+
+lis=new linkedlist()
+lis.push(1)
+lis.push(2)
+lis.push(3)
+lis.push(4)
+
+
+
+lis.traverse()
+
+lis.get(3)
+
+lis.set(3,5)
+
+console.log("\n")
+lis.traverse();
+
+
+
